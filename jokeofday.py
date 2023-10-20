c367f021515b54adc4b2058043c5595d317be6e0
Chad Thompson-Smith <chjthomps@gmail.com>
2023-10-20 08:21:21 -0400

    Update jokeofday.py

--- jokeofday.py
+++ jokeofday.py
@@ -1,6 +1,9 @@
 import requests
 import base64
 from github import Github
+import os
+
+my_secret_key = os.environ['MY_SECRET_KEY']
 
 # Fetch a new joke from JokeAPI
 response = requests.get("https://v2.jokeapi.dev/joke/Programming?format=json")
@@ -8,7 +11,7 @@ joke_data = response.json()
 joke = joke_data['setup'] + " " + joke_data['delivery']
 
 # Fetch the current README.md file from your GitHub repository
-g = Github("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")
+g = Github("my_secret_key")
 repo = g.get_repo("tsmith4014/tsmith4014")
 contents = repo.get_contents("README.md")
 readme_data = base64.b64decode(contents.content).decode("utf-8")
