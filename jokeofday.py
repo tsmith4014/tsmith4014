38ee04e3af90edf271199cdf31f7c703a038262a
Chad Thompson-Smith <chjthomps@gmail.com>
2023-10-20 08:12:11 -0400

    Update jokeofday.py

--- jokeofday.py
+++ jokeofday.py
@@ -8,8 +8,8 @@ joke_data = response.json()
 joke = joke_data['setup'] + " " + joke_data['delivery']
 
 # Fetch the current README.md file from your GitHub repository
-g = Github("YOUR_GITHUB_ACCESS_TOKEN")
-repo = g.get_repo("YOUR_GITHUB_USERNAME/YOUR_REPOSITORY_NAME")
+g = Github("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")
+repo = g.get_repo("tsmith4014/tsmith4014")
 contents = repo.get_contents("README.md")
 readme_data = base64.b64decode(contents.content).decode("utf-8")
 
