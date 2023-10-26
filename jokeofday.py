13c342ed2146d36650f2e01d0355cec0b738cd95
Chad Thompson-Smith <chjthomps@gmail.com>
2023-10-26 15:24:32 -0400

    Update jokeofday.py

--- jokeofday.py
+++ jokeofday.py
@@ -1,7 +1,10 @@
+
+
 import requests
 import base64
 from github import Github
 import os
+import re
 
 # Your GitHub Personal Access Token
 my_secret_key = os.environ['MY_SECRET_KEY']
@@ -13,11 +16,8 @@ joke_data = response.json()
 # Format the joke based on its type
 if joke_data['type'] == 'single':
     joke = joke_data['joke']
-else:  # Assuming the only other type is 'twopart'
+else:
     joke = f"{joke_data['setup']} {joke_data['delivery']}"
-
-print(f"Debug: Received joke data: {joke_data}")
-print(f"Formatted joke: {joke}")
 
 # Fetch the current README.md file from your GitHub repository
 g = Github(my_secret_key)
@@ -25,25 +25,13 @@ repo = g.get_repo("tsmith4014/tsmith4014")
 contents = repo.get_contents("README.md")
 readme_data = base64.b64decode(contents.content).decode("utf-8")
 
-# Find and replace the joke in the README.md file
-readme_lines = readme_data.split('\n')
-for i, line in enumerate(readme_lines):
-    if "⚡ AI Joke of the Day: 🤖" in line:
-        start = line.find("🤖") + len("🤖")  # Find the first 🤖 and move past it
-        end = line.rfind("🤖")  # Find the last 🤖
-        if start != -1 and end != -1 and start != end:
-            new_line = line[:start] + " " + joke + " " + line[end:]
-            readme_lines[i] = new_line
-            break
-else:
-    print("Joke string not found in README.md. No update performed.")
-
-new_readme_data = '\n'.join(readme_lines)
+# Use regex to find and replace the joke in the README.md file
+pattern = r"(⚡ AI Joke of the Day: 🤖 ).*( 🤖)"
+replacement = f"⚡ AI Joke of the Day: 🤖 {joke} 🤖"
+new_readme_data = re.sub(pattern, replacement, readme_data)
 
 # Update the README.md file in your GitHub repository
 repo.update_file(contents.path, "Updated Joke of the Day", new_readme_data, contents.sha)
-
-
 
 
 # import requests
