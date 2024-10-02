7e8f2f49acf043b65bc5df937857a29d9b374b91
Chad Thompson-Smith <chjthomps@gmail.com>
2024-10-02 15:10:29 -0400

    Update suggestionoftheday.py

--- suggestionoftheday.py
+++ suggestionoftheday.py
@@ -8,7 +8,7 @@ my_secret_key = os.environ['MY_SECRET_KEY']
  
 def fetch_suggestion():
     # response = requests.get("https://www.boredapi.com/api/activity")
-    response = requests.get("https://bored-api.appbrewery.com/random")
+    response = requests.get("https://bored-api.appbrewery.com/random?format=json")
     print(response)
     suggestion_data = response.json()
     print(suggestion_data)
